43a3416325c329d749c5bc1b670fd473a49c7dda
Ashwini Nikam <ashu.nikam26@gmail.com>
2023-06-07 09:09:27 -0700

    pali

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const s = str.toLowerCase().replace(/[^a-z]/g, "");
+    return s.split("").reverse().join("") == s;
 };
 
 // Do not edit below this line
