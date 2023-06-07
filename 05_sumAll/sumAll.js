61f9b5497d40f6b8be6ed5e3a298ad4cc772bc16
Ashwini Nikam <ashu.nikam26@gmail.com>
2023-06-07 09:32:30 -0700

    book

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
 const sumAll = function(min, max) {
-    let sum = 0;
-  for (let i = min; i <=max; i++) {
+  if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+  if (min < 0 || max < 0) return "ERROR";
+  if (min > max) {
+    const temp = min;
+    min = max;
+    max = temp;
+  }
+  let sum = 0;
+  for (let i = min; i < max + 1; i++) {
     sum += i;
   }
   return sum;
