7925888bc43a1c1184ff9be96033ef41f95a8415
Ashwini Nikam <ashu.nikam26@gmail.com>
2023-06-07 09:06:09 -0700

    calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,35 @@
-const add = function() {
+const add = function(num1,num2) {
+  return num1+num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1,num2) {
+	return num1-num2;
 	
 };
 
-const sum = function() {
+const sum = function(a) {
+  return a.reduce((total, current) => total + current, 0);
+};
+	
+
+
+const multiply = function(a) {
+  return a.length
+    ? a.reduce((accumulator, nextItem) => accumulator * nextItem)
+    : 0;
+
+};
+
+const power = function(num1,num2) {
+  return Math.pow(num1, num2);
 	
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) return 1;
+  
+  return factorial(n*(n-1));
 	
 };
 
